e50b7a29ccca19f831ec852558a23ce7a28b91ae
taiki numata <numata4171129@gmail.com>
2018-08-21 22:06:32 +0900

    dtoを作成しました。

--- Household/src/dto/House.java
+++ Household/src/dto/House.java
@@ -1,41 +1,38 @@
 package dto;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+public class House {
+	private int number;
+	private String income;
+	private String spending;
 
-/**
- * Servlet implementation class House
- */
-@WebServlet("/House")
-public class House extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public House() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public House(int number,String income,String spending){
+		this.number = number;
+		this.income = income;
+		this.spending = spending;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	}
+	public int getNumber() {
+		return number;
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	public void setNumber(int number) {
+		this.number = number;
 	}
 
-}
+	public String getIncome() {
+		return income;
+	}
+
+	public void setIncome(String income) {
+		this.income = income;
+	}
+
+	public String getSpending() {
+		return spending;
+	}
+
+	public void setSpending(String spending) {
+		this.spending = spending;
+	}
+	}
